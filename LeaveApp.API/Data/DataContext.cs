4a723d167bac05eefce6745ddc4f19019e30e43d
Dumisani Ngcobo <dumisani.ngcobo@bluesp.co.za>
2020-01-05 22:15:57 +0200

    initial commit

---
+++ LeaveApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LeaveApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
